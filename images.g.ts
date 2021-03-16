a76176bcea217f673bb5f667e5d78dd6bc601113
iwearthecrowns <iwearthecrowns@users.noreply.github.com>
2021-03-15 22:57:25 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,36 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 1 1 3 3 3 1 3 
+3 1 3 3 1 3 3 1 3 3 1 3 3 3 1 3 
+3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 3 1 3 3 3 1 3 
+3 1 3 3 1 3 3 3 3 1 1 1 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,34 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile4`, function (sprite, location) {
+    game.over(true)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -200
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`tile2`, function (sprite, location) {
+    game.over(false)
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(11)
+mySprite = sprites.create(img`
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+    3 1 3 3 1 3 3 1 3 1 1 3 3 3 1 3 
+    3 1 3 3 1 3 3 1 3 3 1 3 3 3 1 3 
+    3 1 3 3 1 1 1 3 3 3 1 3 3 3 1 3 
+    3 1 3 3 1 3 3 3 3 3 1 3 3 3 1 3 
+    3 1 3 3 1 3 3 3 3 1 1 1 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 3 3 3 3 3 3 3 3 3 3 3 3 1 3 
+    3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 3 
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+    `, SpriteKind.Player)
+mySprite.ay = 500
+controller.moveSprite(mySprite, 100, 0)
+tiles.setTilemap(tilemap`level`)
+scene.cameraFollowSprite(mySprite)
+tiles.placeOnRandomTile(mySprite, assets.tile`tile3`)
